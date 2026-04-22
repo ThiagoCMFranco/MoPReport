c64c676b13f1f23eb1f6fb11b90b5782e58fcc7e
Thiago Franco <thiagocmfranco@gmail.com>
2026-04-22 00:43:29 -0300

    Issue #47 - Atualizado arquivo TOC para compatibilidade com nova versão.
    Atualizada compatibilidade para versão 12.0.5 do World of Warcraft Retail.

--- Version_Data.lua
+++ Version_Data.lua
@@ -20,5 +20,5 @@
 --
 --------------------------------------------------------------------------------
 
-C_MOPREPORT_VERSION_UID = 1002011
-C_MOPREPORT_VERSION_SEMANTIC_NUMBER = "1.2.11"
+C_MOPREPORT_VERSION_UID = 1002012
+C_MOPREPORT_VERSION_SEMANTIC_NUMBER = "1.2.12"
